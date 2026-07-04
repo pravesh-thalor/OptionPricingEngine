9fbd50f40efc9cf5ead63fa0f645af38dc914c25
pravesh-thalor <bitidprav675@gmail.com>
2026-07-04 11:44:07 +0530

    Update CompilerWarnings.cmake

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -1,1 +1,26 @@
 
+function(target_set_strict_warnings target_name)
+    if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+        target_compile_options(${target_name} PRIVATE
+            -Wall
+            -Wextra
+            -Wpedantic
+            -Wconversion
+            -Wsign-conversion
+            -Wshadow
+            -Wnon-virtual-dtor
+            -Wold-style-cast
+        )
+    elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+        target_compile_options(${target_name} PRIVATE
+            -Wall
+            -Wextra
+            -Wpedantic
+            -Wconversion
+            -Wsign-conversion
+            -Wshadow
+            -Wnon-virtual-dtor
+            -Wold-style-cast
+        )
+    endif()
+endfunction()
